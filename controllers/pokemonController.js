a966613aadf0bded2771eab9002d827307151d39
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-04-16 09:45:13 -0600

    final commit

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -9,13 +9,38 @@ res.render('index.ejs', {
  })
  })
 
+router.get('/new', (req, res) =>{
+res.render('new.ejs')
+})
+
+router.post('/', (req, res) =>{
+pokemon.push(req.body)
+res.redirect('/pokemon');
+})
+
 router.get('/:id', (req,res) => {
 res.render('show.ejs', {
 'pokemon': pokemon[req.params.id]
 })
 }) 
 
+router.delete('/:id', (req, res)=>{
+pokemon.splice(req.params.id, 1);
 
+res.redirect('/pokemon');
+})
+
+router.get('/:id/edit', (req, res) => {
+res.render('edit.ejs', {
+    'pokemon': pokemon[req.params.id],
+    id: req.params.id
+})
+})
+
+router.put('/:id', (req, res) => {
+    pokemon[req.params.id] = req.body;
+    res.redirect('/pokemon');
+})
 
 
 
